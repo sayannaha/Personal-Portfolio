ac35f4cb3f6cc55f47749cb9dec35648a7afaf2b
Sayan Naha <82594248+sayannaha@users.noreply.github.com>
2025-03-20 01:36:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,6 @@ const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
 const link4 = document.getElementById("link4");
-const link5 = document.getElementById("link5");
-const link6 = document.getElementById("link6");
 link1.addEventListener('click', () => {
     scrollToElement('.header');
 });
@@ -21,10 +19,4 @@ link3.addEventListener('click', () => {
 });
 link4.addEventListener('click', () => {
     scrollToElement('.header', 3);
-});
-link5.addEventListener('click', () => {
-    scrollToElement('.header', 4);
-});
-link6.addEventListener('click', () => {
-    scrollToElement('.header', 5);
 });
